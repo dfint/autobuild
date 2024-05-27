b03f488190dfed41c74fa22bfd22a93281781716
insolor <2442833+insolor@users.noreply.github.com>
2024-05-27 17:39:09 +0300

    refactor: Decompose process function

--- automation/automation/process.py
+++ automation/automation/process.py
@@ -46,38 +46,48 @@ async def convert_objects(po_data: bytes, errors_file) -> str:
     return result.getvalue()
 
 
-@logger.catch(reraise=True)
-async def process(language: LanguageInfo, config: Config):
-    csv_directory = config.working_directory / "translation_build" / "csv" / language.name
-    csv_directory.mkdir(parents=True, exist_ok=True)
-
+async def process_hardcoded(directory: Path, language: LanguageInfo, config: Config):
     po_data = await load_file(language_code=language.code, resource_name="hardcoded_steam", config=config)
     csv_data = await convert_hardcoded(po_data)
-    file_path = csv_directory / "dfint_dictionary.csv"
+    file_path = directory / "dfint_dictionary.csv"
+
     async with aiofiles.open(file_path, "wb") as csv_file:
         await csv_file.write(codecs.encode(csv_data, encoding=language.encoding))
 
     logger.info(f"{file_path} written")
+    return csv_data
 
-    csv_with_objects_directory = config.working_directory / "translation_build" / "csv_with_objects" / language.name
-    csv_with_objects_directory.mkdir(parents=True, exist_ok=True)
 
-    errors_file_path = csv_with_objects_directory / "errors.txt"
+async def process_objects(directory: Path, language: LanguageInfo, config: Config):
+    errors_file_path = directory / "errors.txt"
     if errors_file_path.exists():
         errors_file_path.unlink()
 
     po_data = await load_file(language_code=language.code, resource_name="objects", config=config)
 
     with errors_file_path.open("w", encoding="utf-8") as errors_file:
-        csv_with_objects_data = await convert_objects(po_data, errors_file)
+        csv_objects_data = await convert_objects(po_data, errors_file)
 
     if errors_file_path.stat().st_size == 0:
         errors_file_path.unlink()
 
+    return csv_objects_data
+
+
+@logger.catch(reraise=True)
+async def process(language: LanguageInfo, config: Config):
+    csv_directory = config.working_directory / "translation_build" / "csv" / language.name
+    csv_directory.mkdir(parents=True, exist_ok=True)
+    csv_data = await process_hardcoded(csv_directory, language, config)
+
+    csv_with_objects_directory = config.working_directory / "translation_build" / "csv_with_objects" / language.name
+    csv_with_objects_directory.mkdir(parents=True, exist_ok=True)
+    csv_objects_data = await process_objects(csv_with_objects_directory, language, config)
+
     file_path = csv_with_objects_directory / "dfint_dictionary.csv"
     async with aiofiles.open(file_path, "wb") as csv_file:
         await csv_file.write(codecs.encode(csv_data, encoding=language.encoding))
-        await csv_file.write(codecs.encode(csv_with_objects_data, encoding=language.encoding))
+        await csv_file.write(codecs.encode(csv_objects_data, encoding=language.encoding))
 
 
 async def process_all(config: Config):
